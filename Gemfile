ee21b7bc400ea47959ac8b2aadb0fa3e913ded22
Kellar Elliott <kellarelliott@gmail.com>
2020-01-22 22:09:42 +0000

    Mobile CSS

--- Gemfile
+++ Gemfile
@@ -64,5 +64,9 @@ group :test do
   gem 'chromedriver-helper'
 end
 
+group :development, :test do
+  gem 'rspec-rails', '~> 3.5'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_031444) do
+ActiveRecord::Schema.define(version: 2020_01_21_212945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
